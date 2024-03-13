8012639df40e197d3f32bb2cfe5fcea46b8e87b0
su-hak <148946070+su-hak@users.noreply.github.com>
2024-03-13 14:54:00 +0900

    작업환경 1071 x 3094 lolMain 페이지 html + css
    롤 헤더 아이콘 버튼 옆에 드롭다운 메뉴 추가

--- src/main/resources/static/js/main/lolMain.js
+++ src/main/resources/static/js/main/lolMain.js
@@ -13,3 +13,14 @@ $('#sectionA').hover(
         $('#lol_main_img img').css('opacity', '0.7'); // Change opacity back to 100%
     }
 );
+
+
+// 게임 로고 옆 드롭다운 메뉴
+function toggleDropdown() {
+    var dropdownContent = document.getElementById("dropdown-game-body");
+    if (dropdownContent.style.display === "block") {
+        dropdownContent.style.display = "none";
+    } else {
+        dropdownContent.style.display = "block";
+    }
+}
