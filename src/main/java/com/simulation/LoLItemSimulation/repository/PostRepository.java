a5e90274cc7aa18e41099a399c8ce8854edd99c1
Meerkat1030 <hong991000@gmail.com>
2024-03-14 15:26:02 +0900

    [COMMIT] 03/14 commit 게시글 좋아요(완)

--- src/main/java/com/simulation/LoLItemSimulation/repository/PostRepository.java
+++ src/main/java/com/simulation/LoLItemSimulation/repository/PostRepository.java
@@ -5,6 +5,7 @@ import com.simulation.LoLItemSimulation.domain.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -12,4 +13,12 @@ import java.util.List;
 public interface PostRepository extends JpaRepository<Post, Long> {
 
   Page<Post> findAll(Pageable pageable);
+
+  // 게시글 좋아요 갯수
+  @Query("SELECT COUNT(p) FROM Post p")
+  long countAllPosts();
+
+  // 게시글 좋아요 갯수 반환
+  @Query("SELECT COUNT(pl) FROM PostLike pl WHERE pl.post.id = :postId")
+  int getPostLikeCount(Long postId);
 } 
