102a88569c7839ba58a0146fd4ca47af9a39a244
su-hak <148946070+su-hak@users.noreply.github.com>
2024-03-29 09:55:47 +0900

    Update ProbuildController.java

--- src/main/java/com/simulation/LoLItemSimulation/controller/ProbuildController.java
+++ src/main/java/com/simulation/LoLItemSimulation/controller/ProbuildController.java
@@ -4,8 +4,6 @@ import com.simulation.LoLItemSimulation.dto.LeagueEntryDTO;
 import com.simulation.LoLItemSimulation.service.ProbuildService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
-
 @Controller
 public class ProbuildController {
 
@@ -25,55 +22,46 @@ public class ProbuildController {
     }
 
     @GetMapping("/probuild")
-    public String getLeagueInfo(Model model, @RequestParam(defaultValue = "0") int page) {
+    public String getLeagueInfo(Model model/*, @RequestParam(defaultValue = "1") int page*/) {
         // 호출할 때 사용할 큐, 티어, 디비전 정보를 지정합니다
         String queue = "RANKED_SOLO_5x5";
         String tier = "CHALLENGER";
         String division = "I";
         int pageSize = 10;
 
-        // 모든 데이터를 가져옵니다.
-        List<LeagueEntryDTO> allLeagueEntries = getAllLeagueEntries(queue, tier, division);
-
-        // 현재 페이지의 데이터를 가져옵니다.
-        List<LeagueEntryDTO> currentPageEntries = getCurrentPageEntries(allLeagueEntries, page, pageSize);
-
-        model.addAttribute("leagueEntries", currentPageEntries);
-        model.addAttribute("currentPage", page);
-        model.addAttribute("totalPages", calculateTotalPages(allLeagueEntries.size(), pageSize));
-
-
-
         // ProbuildService의 getLeagueInfo 메서드를 호출하여 해당 큐, 티어, 디비전에 속한 모든 정보를 가져옵니다.
-        /*List<LeagueEntryDTO> leagueEntries = new ArrayList<>(); // ArrayList 생성*/
+        List<LeagueEntryDTO> leagueEntries = new ArrayList<>(); // ArrayList 생성
 
-       /* // ProbuildService에서 최대 3개의 배열만 가져오도록 수정
+        // 최대 3번만 요청을 보냅니다.
         LeagueEntryDTO[] entries = probuildService.getLeagueInfo(queue, tier, division);
-        if (entries != null) {
-            for (int i = 0; i < Math.min(3, entries.length); i++) {
-                leagueEntries.add(entries[i]);
+            if (entries != null) {
+                for (int i = 0; i < Math.min(3, entries.length); i++) {
+                    leagueEntries.add(entries[i]);
+                }
+            } else {
+                // 요청이 실패한 경우에 대한 처리를 여기에 추가합니다.
+                // 예: 로깅, 오류 메시지 등
             }
-        } else {
-            // 요청이 실패한 경우에 대한 처리를 여기에 추가합니다.
-            // 예: 로깅, 오류 메시지 등
-        }
-*/
-         // 전체 배열 출력
-        /*for (LeagueEntryDTO entry : probuildService.getLeagueInfo(queue, tier, division)) {
-            leagueEntries.add(entry);
-            System.out.println(leagueEntries + "league fucking entries");
-        }*/
-
 
         // 가져온 정보를 모델에 추가하여 view로 전달합니다.
-        /*model.addAttribute("leagueEntries", leagueEntries);*/
+        model.addAttribute("leagueEntries", leagueEntries);
+
+        // 모든 데이터를 가져옵니다.
+/*        List<LeagueEntryDTO> allLeagueEntries = getAllLeagueEntries(queue, tier, division);*/
+
+        // 현재 페이지의 데이터를 가져옵니다.
+        /*List<LeagueEntryDTO> currentPageEntries = getCurrentPageEntries(allLeagueEntries, page - 1, pageSize);*/
+
+        /*model.addAttribute("leagueEntries", currentPageEntries);
+        model.addAttribute("currentPage", page);
+        model.addAttribute("totalPages", calculateTotalPages(allLeagueEntries.size(), pageSize));*/
 
         // 해당 정보를 보여줄 view의 이름을 반환합니다
         return "probuild";
     }
 
 
-    // 모든 데이터를 가져오는 메서드
+/*    // 모든 데이터를 가져오는 메서드
     private List<LeagueEntryDTO> getAllLeagueEntries(String queue, String tier, String division) {
         // 먼저 전체 데이터를 가져옵니다.
         int limit = Integer.MAX_VALUE; // 제한 없이 모두 가져오기 위해 최대값 설정
@@ -82,7 +70,7 @@ public class ProbuildController {
 
         // 전체 데이터를 리스트로 변환하여 반환합니다.
         return leagueEntriesPage.getContent();
-    }
+    }*/
 
     // 현재 페이지의 데이터를 가져오는 메서드
     private List<LeagueEntryDTO> getCurrentPageEntries(List<LeagueEntryDTO> allEntries, int page, int pageSize) {
