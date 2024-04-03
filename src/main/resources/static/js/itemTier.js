de21c1e80387fff8372c04833c7cc0fe974bb6ee
admin <terious.kr@gmail.com>
2024-04-03 12:36:55 +0900

    아이템티어 페이지 아이템id, 사용횟수 추가

--- src/main/resources/static/js/itemTier.js
+++ src/main/resources/static/js/itemTier.js
@@ -1,4 +1,4 @@
-let apiKey = "RGAPI-31c883a0-8404-44bb-9aaa-4c32d825df69";
+let apiKey = "RGAPI-04a0f0f8-9682-45e4-8b6a-e5d4d894f5bf";
 
 $(document).ready(function() {
     // 챌린저 리그 정보 가져오기
@@ -55,22 +55,16 @@ $(document).ready(function() {
                                             var legendaryItemsUsedInfo = participantInfo.challenges.legendaryItemUsed;
                                             if (legendaryItemsUsedInfo) {
                                                 // 레전더리 아이템 배열에서 각 아이템에 대한 정보 출력
-                                                legendaryItemsUsedInfo.forEach(function(legendaryitemId) {
-                                                    // Riot Games의 데이터 Dragon API를 사용하여 아이템 이름 가져오기
-                                                    getItemName(legendaryitemId, function(itemName) {
-                                                        console.log("championId:", championId, "legendaryItem ID:", legendaryitemId, "itemName:", itemName);
-                                                    });
-
+                                                legendaryItemsUsedInfo.forEach(function(legendaryItemId) {
                                                     // 레전더리 아이템 사용 횟수 추적
-                                                    if (legendaryItemCount.hasOwnProperty(legendaryitemId)) {
-                                                        legendaryItemCount[legendaryitemId]++;
+                                                    if (legendaryItemCount.hasOwnProperty(legendaryItemId)) {
+                                                        legendaryItemCount[legendaryItemId]++;
                                                     } else {
-                                                        legendaryItemCount[legendaryitemId] = 1;
+                                                        legendaryItemCount[legendaryItemId] = 1;
                                                     }
+                                                    // 각 legendaryItemUsed와 해당하는 championId 값 표시
+                                                    console.log("Champion ID:", championId, "Legendary Item ID:", legendaryItemId);
                                                 });
-                                            } else {
-                                                console.log("championId:", championId, "이 참가자가 사용한" +
-                                                    " 전설적인 아이템이 없습니다..");
                                             }
                                         });
                                     }
@@ -83,17 +77,12 @@ $(document).ready(function() {
                     }
                 });
             });
+
             // 모든 AJAX 요청이 완료된 후 실행되는 코드
             $(document).ajaxStop(function() {
-                // 레전더리 아이템 사용 횟수를 표시
-                console.log("Legendary Item Usage Count:");
-                for (var itemId in legendaryItemCount) {
-                    if (legendaryItemCount.hasOwnProperty(itemId)) {
-                        console.log("Item ID:", itemId, "Usage Count:", legendaryItemCount[itemId]);
-                    }
-                }
+                // 데이터 수집 및 토스트 그리드에 표시하는 함수 호출
+                displayLegendaryItemUsage(legendaryItemCount);
             });
-
         },
         error: function(xhr, status, error) {
             console.error('API 요청 중 오류 발생:', status, error);
@@ -101,20 +90,41 @@ $(document).ready(function() {
     });
 });
 
-// Riot Games의 데이터 Dragon API를 사용하여 아이템 이름을 가져오는 함수
-function getItemName(itemId, callback) {
-    // Riot Games의 데이터 Dragon API에서 아이템 이름 가져오기
-    $.ajax({
-        url: 'https://ddragon.leagueoflegends.com/cdn/14.6.1/data/ko_KR/item.json',
-        type: 'GET',
-        success: function(itemData) {
-            // 아이템 ID에 해당하는 아이템 이름 찾기
-            var itemName = itemData.data[itemId].name;
-            // 콜백 함수 호출하여 아이템 이름 전달
-            callback(itemName);
-        },
-        error: function(xhr, status, error) {
-            console.error('Failed to fetch item data:', error);
+// 토스트 그리드에 레전더리 아이템 사용 횟수를 표시하는 함수
+function displayLegendaryItemUsage(legendaryItemCount) {
+    // 토스트 그리드로 데이터 전송 또는 표시하는 로직을 여기에 작성
+    console.log("Legendary Item Usage Count:");
+
+    // 데이터를 담을 배열 초기화
+    var gridData = [];
+
+    // 데이터 배열에 행 추가
+    for (var itemId in legendaryItemCount) {
+        if (legendaryItemCount.hasOwnProperty(itemId)) {
+            console.log("Item ID:", itemId, "Usage Count:", legendaryItemCount[itemId]);
+            // 토스트 그리드에 표시될 데이터 객체 생성
+            var rowData = { itemId: itemId, usageCount: legendaryItemCount[itemId] };
+            // 데이터 배열에 행 추가
+            gridData.push(rowData);
         }
+    }
+
+    // 그리드 생성 및 데이터 전달
+    const grid = new tui.Grid({
+        el: document.getElementById('grid'),
+        data: gridData,
+        scrollX: false,
+        scrollY: false,
+        columns: [
+            {
+                header: 'Item ID',
+                name: 'itemId'
+            },
+            {
+                header: 'Usage Count',
+                name: 'usageCount'
+            }
+        ]
     });
 }
+
