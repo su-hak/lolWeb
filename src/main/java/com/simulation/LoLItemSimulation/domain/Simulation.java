4cb54836c47e4f15a13eb5ce0dab959898988e6e
Meerkat1030 <hong991000@gmail.com>
2024-04-18 12:27:49 +0900

    [COMMIT] 04/18 commit simulation공유 기능 개발중

--- src/main/java/com/simulation/LoLItemSimulation/domain/Simulation.java
+++ src/main/java/com/simulation/LoLItemSimulation/domain/Simulation.java
@@ -7,10 +7,6 @@ import lombok.Setter;
 import lombok.ToString;
 
 @Entity
-@NoArgsConstructor
-@Setter
-@Getter
-@ToString
 public class Simulation {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -43,4 +39,84 @@ public class Simulation {
     @ManyToOne
     @JoinColumn(name = "post_id", insertable = false, updatable = false)
     private Post post; // 포스트 객체를 참조할 수도 있습니다.
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getPostId() {
+        return postId;
+    }
+
+    public void setPostId(Long postId) {
+        this.postId = postId;
+    }
+
+    public String getChampionName() {
+        return championName;
+    }
+
+    public void setChampionName(String championName) {
+        this.championName = championName;
+    }
+
+    public String getItem1() {
+        return item1;
+    }
+
+    public void setItem1(String item1) {
+        this.item1 = item1;
+    }
+
+    public String getItem2() {
+        return item2;
+    }
+
+    public void setItem2(String item2) {
+        this.item2 = item2;
+    }
+
+    public String getItem3() {
+        return item3;
+    }
+
+    public void setItem3(String item3) {
+        this.item3 = item3;
+    }
+
+    public String getItem4() {
+        return item4;
+    }
+
+    public void setItem4(String item4) {
+        this.item4 = item4;
+    }
+
+    public String getItem5() {
+        return item5;
+    }
+
+    public void setItem5(String item5) {
+        this.item5 = item5;
+    }
+
+    public String getItem6() {
+        return item6;
+    }
+
+    public void setItem6(String item6) {
+        this.item6 = item6;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
 }
